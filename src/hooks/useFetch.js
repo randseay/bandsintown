4624d96f540649f2cc8d0c9ea502bf169f22457f
Rand Seay <rand.seay@gmail.com>
2022-09-06 22:52:19 -0700

    Create a hook for fetching.

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+function useFetch(url) {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(url);
+      const data = await response.json();
+
+      setData(data);
+    }
+
+    fetchData().catch(setError);
+  }, []);
+
+  return {
+    data,
+    error,
+  };
+}
+
+export default useFetch;
